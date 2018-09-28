38b191fb0414fb7988beeabc45c43d89ac49c02e
Thomas Ward <teward@ubuntu.com>
2018-09-28 16:15:54 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
                      "build DMARC-compliant messages with origin points of the ListServ itself from original messages "
                      "that a ListServ would receive.",
     license='AGPLv3+',
-    url='https://github.com/teward/dmarcmsg',
+    url='https://gitlab.com/teward/dmarcmsg',
     download_url='https://pypi.python.org/pypi/dmarcmsg',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -28,13 +28,9 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
         'Topic :: Communications :: Email',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
